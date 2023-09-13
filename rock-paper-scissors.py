32fb75c14e4c7a50371ea2971ecc039eb9b43de7
sylla-seiku <seikusylla@gmail.com>
2023-09-13 16:17:31 -0400

    Creating A Game.

---
+++ rock-paper-scissors.py
@@ -0,0 +1,19 @@
+#creating a game.
+import random
+
+computer_choice= random.choice(['rock', 'paper', 'scissors'])
+user_choice = input('Pick rock, paper, or scissors? ')
+
+print('Computer choice:', computer_choice)
+
+
+if computer_choice == user_choice:
+    print('Tie.')
+elif user_choice == 'rock' and computer_choice == 'scissors':
+    print('You win!')
+elif user_choice == 'paper' and computer_choice == 'rock':
+    print('You win!')
+elif user_choice == 'scissors' and computer_choice == 'paper':
+    print('You win!')
+else:
+    print('You lose, Computer win!')
